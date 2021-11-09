27a060fd83a0d16a75989112d0a9321bfb10682a
Mohamed Keridou <johndoe@example.com>
2021-11-09 18:04:35 +0100

    done

---
+++ src/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+
+    return (
+        <>
+        <nav className="navbar navbar-dark bg-primary mb-3">
+        <div className="container">
+            <NavLink class="navbar-brand" to="/">
+                WikiCountries
+            </NavLink>
+        </div>
+    </nav>
+    </>
+    )
+}
+
+export default Navbar
+
